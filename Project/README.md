d7425d8bc644b5b75c9fc972d55d5c2045994b95
Pawan Kumar Rai <ghz.rai@gmail.com>
2017-07-14 19:19:57 +0530

    Live project
    project movie with code comment added

---
+++ Project/README.md
@@ -0,0 +1,10 @@
+# Movies project
+
+# Steps to run project
+    # Unzip the folder on local machine
+    # import or open the project folder in Atom or any other editor
+    # Go to "fresh_tomatoes.py" file
+    # Run the module
+
+# It should a movies catalogue page with Movies list
+# Hover on any of the movie and click to play the trailor
--- Project/movies/entertainment_center.py
+++ Project/movies/entertainment_center.py
@@ -1,30 +1,21 @@
 import media
 import fresh_tomatoes
 
+# Make movie instances
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-
-#print (toy_story.storyline)
 
 avtar = media.Movie("Avtar", "A marine on a alien planet", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
-#print (avtar.storyline)
-#avtar.show_trailer()
-
 school_of_rock = media.Movie("School of Rock", "Storyline", "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg", "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
-
 
 ratatouille = media.Movie("Ratatouille", "Storyline", "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg", "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
-
 midnight_in_paris = media.Movie("Midnight in Paris", "Storyline", "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg", "https://www.youtube.com/watch?v=BYRWfS2s2v4")
-
 
 hunger_games = media.Movie("Hunger Games", "Storyline", "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg", "https://www.youtube.com/watch?v=mfmrPu43DF8")
 
-
-# open webbrowser
+# Collect all the movie instances in list
 movies = [toy_story, avtar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
 
+# Pass the movies list to open movie catalouge
 fresh_tomatoes.open_movies_page(movies)
-
-print (media.Movie.VALID_RATINGS)
