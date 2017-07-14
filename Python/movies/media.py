e895a422eeeec9860eed78e830bd2c3962653fac
Pawan Kumar Rai <ghz.rai@gmail.com>
2017-07-14 16:51:03 +0530

    init commit

--- Python/movies/media.py
+++ Python/movies/media.py
@@ -1,7 +1,8 @@
 import webbrowser
 
 class Movie():
-    """ This class provides a way to store movie related information""" 
+    """ This class provides a way to store movie related information"""
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     # init
